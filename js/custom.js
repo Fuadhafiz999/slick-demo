debebf91a00f8441a2879a041d30ce489ddcfd27
Fuadhafiz999 <ahshanhafizfuad@gmail.com>
2023-06-28 03:46:09 +0600

    demo

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+$(function () {
+    // slick start
+
+    $('.slick-slider-main').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow: ".prev",
+        nextArrow: ".next"
+
+      });
+
+
+
+
+
+
+
+})
